801a52ab203f88ed482ba31c5f46ae02d591064d
jessica.pei <jessica.pei@imaginuity.com>
2016-05-27 15:15:52 -0500

    ACF plugin

---
+++ themes/sage-8.4.2/templates/page-header.php
@@ -0,0 +1,9 @@
+<?php use Roots\Sage\Titles; ?>
+
+<?php if (is_front_page()) { ?>
+    <h1><?php the_field('welcome_h1'); ?></h1>
+<?php } else { ?>
+  <div class="page-header">
+    <h1><?= Titles\title(); ?></h1>
+  </div>
+<?php } ?>
