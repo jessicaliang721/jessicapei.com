6b4470054cdda3b659e1cc0360db85843d9321c1
jessica.pei <jessica.pei@imaginuity.com>
2016-05-31 13:58:41 -0500

    Added 'work' section

--- themes/sage-8.4.2/templates/content-front-page.php
+++ themes/sage-8.4.2/templates/content-front-page.php
@@ -23,7 +23,35 @@
 </section>
 
 <!--My Work-->
-<section>
-    
+<section class="work">
+    <h3><?php the_field("work_h3"); ?></h3>
+    <div class="row text-center">
+        <?php
+        $fields = CFS()->get( 'work' );
+        foreach ( $fields as $field ) {?>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <?php
+                $values = CFS()->get( 'work_url' );
+                foreach ( $values as $post_id ) {
+                    $the_post = get_post( $post_id );
+                    var_dump($values);
+                    echo $the_post->post_title;
+                } ?>
+                <a href=""><img src="<?php echo $field['work_image']; ?>"></a>
+                <p><?php echo $field['work_title']; ?></p>
+            </div>
+        <?php } ?>
+    </div>
+    <div class="row text-center">
+        <button>View More</button>
+    </div>
 </section>
-<!--Link to Resume-->
+
+<section>
+    <h3><?php the_field("contact_h3"); ?></h3>
+    <div class="row">
+        <div class="col-xs-12 col-md-6">
+            <?php the_field("contact_summary"); ?>
+        </div>
+    </div>
+</section>
