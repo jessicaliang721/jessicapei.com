89ef78a82f1deb3ff878cae2ea3c5f092dbc7a5d
jessica.pei <jessica.pei@imaginuity.com>
2016-06-01 16:29:49 -0500

    create theme options page

--- themes/sage-8.4.2/functions.php
+++ themes/sage-8.4.2/functions.php
@@ -27,3 +27,37 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+//Admin Menu
+/** Step 2 (from text above). */
+add_action('admin_menu', 'add_global_custom_options');
+
+/** Step 1. */
+function add_global_custom_options()
+{
+  add_options_page('Global Custom Options', 'Global Custom Options', 'manage_options', 'functions','global_custom_options');
+}
+
+/** Step 3. */
+function global_custom_options()
+{
+  ?>
+  <div class="wrap">
+    <h2>Theme Options</h2>
+    <form method="post" action="options.php">
+      <?php wp_nonce_field('update-options') ?>
+      <p><strong>Twitter ID:</strong><br />
+        <input type="text" name="twitterid" size="45" value="<?php echo get_option('twitterid'); ?>" />
+      </p>
+      <p><input type="submit" name="Submit" value="Store Options" /></p>
+      <input type="hidden" name="action" value="update" />
+      <p><strong>Facebook Page Links:</strong><br />
+        <input type="text" name="fb_link" size="45" value="<?php echo get_option('fb_link'); ?>" />
+      </p>
+      <p><input type="submit" name="Submit" value="Store Options" /></p>
+      <input type="hidden" name="action" value="update" />
+      <input type="hidden" name="page_options" value="twitterid,fb_link" />
+    </form>
+  </div>
+  <?php
+}
