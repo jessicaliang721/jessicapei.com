ebd6840553f7da0cd55a4fb3718a25d16e453d67
jessica.pei <jessica.pei@imaginuity.com>
2016-06-02 12:39:39 -0500

    splash background

--- themes/sage-8.4.2/templates/page-header.php
+++ themes/sage-8.4.2/templates/page-header.php
@@ -1,8 +1,11 @@
 <?php use Roots\Sage\Titles; ?>
 
 <?php if (is_front_page()) { ?>
+  <div class="text-center container-fluid splash-background" style="background-image: url(<?php the_field('welcome_background'); ?>)">
     <h1><?php the_field('welcome_h1'); ?></h1>
     <h2><?php the_field('welcome_subtitle'); ?></h2>
+  </div>
+
 <?php } else { ?>
   <div class="page-header">
     <h1><?= Titles\title(); ?></h1>
