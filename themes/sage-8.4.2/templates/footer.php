c0b89b12394bf2a16be331531791f9fe298e4e7b
jessica.pei <jessica.pei@imaginuity.com>
2016-06-07 15:39:39 -0500

    theme options plugin

--- themes/sage-8.4.2/templates/footer.php
+++ themes/sage-8.4.2/templates/footer.php
@@ -7,10 +7,13 @@
       <p>Copyright &copy Jessica Pei <?php echo date('Y'); ?></p>
       <ul>
         <li>
-          <a href="http://twitter.com/<?php echo get_option('twitterid'); ?>">Twitter</a>
+          <a href="http://github.com/<?php echo get_option('github_handle'); ?>">Github</a>
         </li>
         <li>
-          <a href="http://facebook.com/<?php echo get_option('fb_link'); ?>">Facebook</a>
+          <a href="http://instagram.com/<?php echo get_option('instagram_handle'); ?>">Instagram</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/<?php echo of_get_option('linkedin_handle'); ?>"><img src="<?php echo of_get_option('linkedin_icon');?>"></a>
         </li>
       </ul>
 
