2303fa2383c2cc19a85bdde45aafbf700bd95710
jessica.pei <jessica.pei@imaginuity.com>
2016-06-08 16:04:47 -0500

    content-single styling

--- themes/sage-8.4.2/templates/content-single.php
+++ themes/sage-8.4.2/templates/content-single.php
@@ -1,15 +1,17 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
+    <header class="container-fluid text-center section-background" style="background-image: url(<?php the_field('work_background'); ?>)">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+<!--      --><?php //get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
-      <?php the_content(); ?>
+      <div class="container">
+        <?php the_content(); ?>
+      </div>
     </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
+<!--    <footer>-->
+<!--      --><?php //wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<!--    </footer>-->
     <?php comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
--- themes/sage-8.4.2/templates/header.php
+++ themes/sage-8.4.2/templates/header.php
@@ -13,7 +13,7 @@
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav pull-right']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
