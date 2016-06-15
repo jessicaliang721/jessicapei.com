0dee06dac703315723c5411f8abdd54a4b0380b5
jessica.pei <jessica.pei@imaginuity.com>
2016-06-15 09:35:22 -0500

    update social with icons

--- themes/sage-8.4.2/templates/social.php
+++ themes/sage-8.4.2/templates/social.php
@@ -1,16 +1,14 @@
 <ul class="social-icons">
     <li>
-        <a href="http://github.com/<?php echo of_get_option('github_handle'); ?>" target="_blank">Github</a>
+        <a href="http://github.com/<?php echo of_get_option('github_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('github_icon'); ?>"></a>
     </li>
     <li>
-        <a href="http://instagram.com/<?php echo of_get_option('instagram_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('instagram_icon'); ?>"</a>
+        <a href="http://instagram.com/<?php echo of_get_option('instagram_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('instagram_icon'); ?>"></a>
     </li>
     <li>
         <a href="https://www.linkedin.com/in/<?php echo of_get_option('linkedin_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('linkedin_icon');?>"></a>
     </li>
     <li>
-        <a href="mailto:jessicapei93@gmail.com">
-            Email
-        </a>
+        <a href="mailto:jessicapei93@gmail.com"><img src="<?php echo of_get_option('email_icon'); ?>"></a>
     </li>
 </ul>
