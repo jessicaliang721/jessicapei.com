2b44be00c8e7e723dcce6a5fc66a3389269cd1a0
jessica.pei <jessica.pei@imaginuity.com>
2016-06-15 09:33:04 -0500

    download wp migration plugin

--- themes/sage-8.4.2/templates/social.php
+++ themes/sage-8.4.2/templates/social.php
@@ -3,7 +3,7 @@
         <a href="http://github.com/<?php echo of_get_option('github_handle'); ?>" target="_blank">Github</a>
     </li>
     <li>
-        <a href="http://instagram.com/<?php echo of_get_option('instagram_handle'); ?>" target="_blank">Instagram</a>
+        <a href="http://instagram.com/<?php echo of_get_option('instagram_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('instagram_icon'); ?>"</a>
     </li>
     <li>
         <a href="https://www.linkedin.com/in/<?php echo of_get_option('linkedin_handle'); ?>" target="_blank"><img src="<?php echo of_get_option('linkedin_icon');?>"></a>
